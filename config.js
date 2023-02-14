00ac48085a9b19fac0c0a04050c076a2006379f4
uznplay <94210874+uznplay@users.noreply.github.com>
2023-02-14 21:24:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Happy Birthday';
 const giftUrl = 'https://www.facebook.com/profile.php?id=100073077698440';
 const eventName = 'Chúc cậu sinh nhật vui vẻ';
 const titleCard = 'Humnmnm';
-const contentCard = 'Thực sự thì tớ cũng không biết nói gì hơn cả,nhưng mà chúc cậu tuổi 18 luôn vui vẻ,tự tin hơn về bản thân mình,sắp tới thi cử thuộm buồm xuôi gió đạt đúng nguyện vọng của mình....(quan trọng nhất thì là nghe lời tớ 1 xíu , đừng có mà bướng :)(';
+const contentCard = 'Thực sự thì tớ cũng không biết nói gì hơn cả,nhưng mà chúc cậu tuổi 18 luôn vui vẻ,tự tin hơn về bản thân mình,sắp tới thi cử thuộm buồn xuôi gió đạt đúng nguyện vọng của mình....(quan trọng nhất thì là nghe lời tớ 1 xíu , đừng có mà bướng :)(';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
